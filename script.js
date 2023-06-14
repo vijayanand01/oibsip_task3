2ac572cf6127980cfdf4fcccec695457da11ac72
SHORALLY VIJAYANAND <svijayanand140704@gmail.com>
2023-06-14 23:58:02 +0530

    TASK3

---
+++ script.js
@@ -0,0 +1,49 @@
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInputtitle").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  var tx = document.createTextNode(
+    "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0"
+  );
+  li.appendChild(tx);
+  var inputValuedetail = document.getElementById("myInputDetail").value;
+  var tx = document.createTextNode(inputValuedetail);
+  li.appendChild(tx);
+  if (inputValue === "" || inputValuedetail === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInputtitle").value = "";
+  document.getElementById("myInputDetail").value = "";
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
